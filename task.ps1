fbe09236e8b625f86219e8c628aa77f93d531f1c
Danylo Povar <danila74povar@gmail.com>
2026-02-11 21:58:10 +0200

    azure_task8

--- task.ps1
+++ task.ps1
@@ -1,1 +1,11 @@
 # Write your code here
+
+$resourceGroup = "MATE-AZURE-TASK-5"
+
+$disks = Get-AzDisk -ResourceGroupName $resourceGroup
+
+$unattachedDisks = $disks | Where-Object { $_.DiskState -eq "Unattached" }
+
+Write-Host "Знайдено непідключених дисків: $($unattachedDisks.Count)" -ForegroundColor Green
+
+$unattachedDisks | ConvertTo-Json -Depth 5 | Set-Content -Path "result.json"
